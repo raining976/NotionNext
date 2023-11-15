c62aec9d97827501f5ac50199cb0ec04d799b64d
raining <running.zhang@foxmail.com>
2023-11-15 15:13:57 +0800

    feat:添加鼠标点击弹出文字的脚本

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,2 +1,7 @@
+import { fnTextPopup } from "./custom/textPopup"
+
 // 这里编写自定义js脚本；将被静态引入到页面中
 document.querySelector("#home-nav-button").style.display = "none"
+
+
+fnTextPopup(['❤富强❤', '❤民主❤', '❤文明❤', '❤和谐❤', '❤自由❤', '❤平等❤', '❤公正❤', '❤法治❤', '❤爱国❤', '❤敬业❤', '❤诚信❤', '❤友善❤'])
